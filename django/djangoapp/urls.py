4c115cc8c79dba24b57d3029f8177c267528aae4
Jithin Jacob Issac <160052252+jithinjissac@users.noreply.github.com>
2024-03-07 11:43:29 +0000

    new

--- django/djangoapp/urls.py
+++ django/djangoapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('login.html', views.login, name='login'),
     path('register.html', views.register, name='register'),
     path('adminis/dashboard.html', views.admindashboard, name='admindashboard'),
+    path('adminis/addcategory.html', views.adminaddcategory, name='adminaddcategory'),
+    path('adminis/reviewproduct.html', views.adminreviewproduct, name='adminreviewproduct'),
+    path('adminis/viewproduct.html', views.adminviewproduct, name='adminviewproduct'),
+
 ]
--- django/djangoapp/views.py
+++ django/djangoapp/views.py
@@ -11,3 +11,9 @@ def register (request):
     return render(request, 'register.html')
 def admindashboard (request): 
     return render(request, 'adminis/dashboard.html')
+def adminaddcategory (request): 
+    return render(request, 'adminis/addcategory.html')
+def adminreviewproduct (request): 
+    return render(request, 'adminis/reviewproduct.html')
+def adminviewproduct (request): 
+    return render(request, 'adminis/viewproduct.html')
