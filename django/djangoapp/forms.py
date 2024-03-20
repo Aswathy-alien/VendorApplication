c4d6d9120f6e29c18a575cf0fe1f7f3d6ea20238
Jithin Jacob Issac <160052252+jithinjissac@users.noreply.github.com>
2024-03-20 23:35:09 +0000

    Changes

--- django/djangoapp/forms.py
+++ django/djangoapp/forms.py
@@ -1,2 +1,8 @@
 
 from django import forms
+from .models import ProductCategory
+
+class ProductCategoryForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ['category_name', 'category_description']
