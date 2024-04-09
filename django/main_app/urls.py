4550d0f3293d56510420daa4a52b663c443160f4
Aswathy-alien <105476280+Aswathy-alien@users.noreply.github.com>
2024-04-09 14:59:53 +0100

    updates
    updates

--- django/main_app/urls.py
+++ django/main_app/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from . import views
 from django.urls import path
 
@@ -11,3 +12,20 @@ urlpatterns = [
     path('productlisting.html', views.product_listing, name='product_listing'),
     path('profilepage.html', views.profile_page, name='profile_page')
 ]
+=======
+from . import views
+from django.urls import path
+from django.conf.urls.static import static  # new
+from django.conf import settings  # new
+app_name = 'main_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('login.html', views.login, name='login'),
+    path('register.html', views.register, name='register'),
+    path('productlisting.html', views.product_listing, name='product_listing'),
+    path('profilepage.html', views.profile_page, name='profile_page')
+]
+
+>>>>>>> Stashed changes
